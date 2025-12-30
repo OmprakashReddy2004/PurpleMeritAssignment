6362f3d5280d224831f1d834e59c66d7e7817670
Om prakash reddy <omprakashreddyhr@gmail.com>
2025-12-30 23:01:58 +0530

    updated settings.py for CORS origin listening

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -53,11 +53,7 @@ MIDDLEWARE = [
 # -------------------------------------------------
 # CORS
 # -------------------------------------------------
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # -------------------------------------------------
